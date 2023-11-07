21f1feee84df265ff1e5bae62f1126c48a5c730e
Daniel L. Lau <dllau@engr.uky.edu>
2023-11-07 10:03:25 -0500

    small fixes

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setOrganizationName(QString("Lau Consulting Inc"));
+    a.setOrganizationDomain(QString("drhalftone.com"));
+    a.setApplicationName(QString("LAUYoloPoseLabeler"));
 
     LAUYoloPoseLabelerWidget w;
     if (w.isValid()){
